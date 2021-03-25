fe5635f947c7d599c73627abd27ef71fd0e8c5ba
Mr.ASCII <chenhangxxx@gmail.com>
2021-03-25 16:11:22 +0800

    Create hello_world.js

---
+++ QuantumultX/hello_world.js
@@ -0,0 +1,12 @@
+var ourDog = {
+    name: "Sunert",
+    legs: 4,
+    tails: 1,
+    friends: ["dog1","dog2"],
+    "description": "来源:https://github.com/"
+};
+
+//Change this value to test 
+// console.log(ourDog);
+// ourDog.head = 1;
+console.log(ourDog.name);
